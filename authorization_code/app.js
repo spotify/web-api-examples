dc0ae0e497cefe5729854c8b28614ce745792a96
Pontus Alexander <pontus.alexander@gmail.com>
2018-01-24 22:10:48 +0100

    Pass on full body on `/refresh_token` success.
    The success message on refresh may contain a new refresh token
    and an updated expire time which is obscured by the filtering.

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -133,10 +133,7 @@ app.get('/refresh_token', function(req, res) {
 
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      var access_token = body.access_token;
-      res.send({
-        'access_token': access_token
-      });
+      res.send(body);
     }
   });
 });
