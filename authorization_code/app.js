caf20da3dd674f1ddebf623ec8218e1566edc923
menef0124 <menefee0124@gmail.com>
2021-06-24 09:35:28 -0500

    Initial commit

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -12,10 +12,11 @@ var request = require('request'); // "Request" library
 var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
+var credJson = require("../credentials.json");
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = credJson['client-id']; // Your client id
+var client_secret = credJson['client-secret']; // Your secret
+var redirect_uri = credJson['callback-uri']; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
