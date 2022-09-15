0bb3f2ebba204015d026e619a2746b37d1cc5a34
John Mangel <john.m5321@yahoo.com>
2022-09-15 10:11:32 -0700

    store refresh_token in cookie instead of with access_token in query string
    It's not secure to display the token in the query string.
    This way, the access_token is only ever stored in memory (and sent to the frontend),
    and if the page is refreshed the frontend immediately uses the cookie-stored
    refresh_token to get the access token again

--- app.js
+++ app.js
@@ -90,26 +90,18 @@ app.get('/callback', function(req, res) {
     request.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200) {
 
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
+        let refresh_token = body.refresh_token;
 
-        var options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
+        const expiration = process.env.REFRESH_TOKEN_COOKIE_EXPIRATION || 604800000;
 
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          console.log(body);
-        });
+        res.cookie('refresh_token', refresh_token, {
+          expires: new Date(Date.now() + expiration),
+          secure: true,
+          httpOnly: true,
+          sameSite: 'strict'
+        })
 
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
+        res.redirect('/');
       } else {
         res.redirect('/#' +
           querystring.stringify({
@@ -123,7 +115,12 @@ app.get('/callback', function(req, res) {
 app.get('/refresh_token', function(req, res) {
 
   // requesting access token from refresh token
-  var refresh_token = req.query.refresh_token;
+  var refresh_token = req.cookies.refresh_token;
+
+  if (!refresh_token) {
+    return res.send({});
+  }
+
   var authOptions = {
     url: 'https://accounts.spotify.com/api/token',
     headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
