f9bac4c5007c34f052f402581e8abea17c29bbda
jo <jfranchetti@spotify.com>
2023-01-25 14:13:26 +0000

    add new demo for howto guide, move authentication demos into their own folder

---
+++ get_user_profile/readme.md
@@ -0,0 +1,13 @@
+
+# Display your Spotify Profile Data in a Web App
+
+This is the final code for the Spotify Web API - How to Display your Profile Data in a Web . You can run this demo directly or [walk through the tutorial](https://developer.spotify.com/documentation/web-api/howto/web-app-profile).
+
+## Usage
+
+To run this demo clone the repository, cd into the `get_user_profile` directory and run:
+
+```bash
+npm install
+npm run dev
+```
---
+++ get_user_profile/src/types.d.ts
@@ -0,0 +1,23 @@
+interface UserProfile {
+    country: string;
+    display_name: string;
+    email: string;
+    explicit_content: {
+        filter_enabled: boolean,
+        filter_locked: boolean
+    },
+    external_urls: { spotify: string; };
+    followers: { href: string; total: number; };
+    href: string;
+    id: string;
+    images: Image[];
+    product: string;
+    type: string;
+    uri: string;
+}
+
+interface Image {
+    url: string;
+    height: number;
+    width: number;
+}
