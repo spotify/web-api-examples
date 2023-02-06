4290133be8ad02582e6f39ac6e6baead83ebd7a9
John Mangel <john.m5321@yahoo.com>
2023-02-06 12:56:50 -0500

    get permission to modify playback

--- server/app.js
+++ server/app.js
@@ -48,7 +48,7 @@ app.get('/login', function(req, res) {
   res.cookie(stateKey, state);
 
   // your application requests authorization
-  var scope = 'user-read-private user-read-email playlist-read-private';
+  var scope = 'user-read-private user-read-email playlist-read-private user-read-playback-state user-modify-playback-state';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
